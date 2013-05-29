fad75d4205daad93f16f33f9df5a71f7db223286
Edgar Guerra <ed.guerra.de@gmail.com>
2013-05-29 22:41:11 +0200

    Defined DB connection

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
+			'database'  => 'hookydb',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => '12346',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
